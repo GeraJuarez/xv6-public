d3f04fa11ac6a45cc2b4a36aa4622623c1aa58d9
geraJuarez <geratrex781@gmail.com>
2017-10-25 20:34:14 -0500

    Commented rename

--- rename.c
+++ rename.c
@@ -5,16 +5,19 @@
 int
 main(int argc, char *argv[])
 {
+    // Exit if does not have enough parameters
     if(argc != 3){
         printf(2, "Usage: rename old_name new_name\n");
         exit();
     }
 
+    // Create a link with a different name
     if(link(argv[1], argv[2]) < 0) {
         printf(2, "rename %s %s: failed\n", argv[1], argv[2]);
         exit();
     }
   
+    // Delete the previous link with the old name
     if(unlink(argv[1]) < 0){
         printf(2, "rename: %s failed to rename\n", argv[1]);
         exit();
